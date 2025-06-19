b5225f9afd72e9eddb592d3f3e468c7bddd1aebd
Safak <safakustatr@gmail.com>
2025-06-19 12:44:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ money_machine = MoneyMachine()
 is_machine_on = True
 
 while is_machine_on:
-	choice = input(f"What would you like to do? {menu.get_items()}")
-	if choice == "off":
-		is_machine_on = False
-	elif choice == "report":
-		coffee_maker.report()
-		money_machine.report()
-	else:
-		drink = menu.find_drink(choice)
-		if coffee_maker.is_resource_sufficient(drink):
-			if money_machine.make_payment(drink.cost):
-				coffee_maker.make_coffee(drink)
+    choice = input(f"What would you like to do? {menu.get_items()}")
+    if choice == "off":
+	is_machine_on = False
+    elif choice == "report":
+	coffee_maker.report()
+	money_machine.report()
+    else:
+	drink = menu.find_drink(choice)
+	if coffee_maker.is_resource_sufficient(drink):
+	    if money_machine.make_payment(drink.cost):
+	        coffee_maker.make_coffee(drink)
